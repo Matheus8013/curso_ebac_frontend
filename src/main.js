0fe7934e63e623cd4d8f08ab0718957b80fbc108
Matheus8013 <132845674+Matheus8013@users.noreply.github.com>
2025-02-13 19:17:21 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,41 @@
+const listaDeAlunaosENotas = [
+    {
+        nome : 'Ana', nota : 8
+    },
+    {
+        nome : 'Bruno', nota : 9
+    },
+    {
+        nome : 'Beatriz', nota : 6
+    },
+    {
+        nome : 'Nicolas', nota : 3
+    },
+    {
+        nome : 'Jorge', nota : 5
+    },
+    {
+        nome : 'Amanda', nota : 6
+    },
+    {
+        nome : 'Ricardo', nota : 4
+    },
+    {
+        nome : 'Bruna', nota : 7
+    },
+]
+
+const relatorioAlunosAprovados = listaDeAlunaosENotas.filter((item) => {
+    return item.nota >= 6
+});
+
+const relatorioAlunosReprovados = listaDeAlunaosENotas.filter((item) => {
+    return item.nota < 6
+});
+
+console.log('Estes foram os alunos aprovados:')
+console.log(relatorioAlunosAprovados)
+
+console.log('Estes foram os alunos reprovados:')
+console.log(relatorioAlunosReprovados)
+
